2d04baa964c7c6aaffe69ee9d95ac4657314c621
Acervans <javierwang01052001@gmail.com>
2023-11-26 02:44:01 +0100

    Remove unused imports

--- DoodlEmotion.py
+++ DoodlEmotion.py
@@ -1,9 +1,5 @@
 from keras.models import load_model
 from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg16 import decode_predictions
-from keras.applications.vgg16 import VGG16
 import numpy as np
 import os
 
